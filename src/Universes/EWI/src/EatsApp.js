d056ed278716db7472e3660d42078b1f6849c239
ValerySM <valerysokm@gmail.com>
2024-09-02 15:56:49 +0300

    Update EatsApp.js

--- src/Universes/EWI/src/EatsApp.js
+++ src/Universes/EWI/src/EatsApp.js
@@ -10,13 +10,6 @@ import clickerImage from '../public/clicker-image.png'
 import SoonTab from './components/SoonTab'
 import UniverseData from './UniverseData';
 
-import {
-  handleClick,
-  handleDamageUpgrade,
-  handleEnergyUpgrade,
-  handleRegenUpgrade
-} from './scripts/functions';
-
 const DamageIndicator = ({ x, y, damage }) => (
   <div className="damage-indicator" style={{ left: x, top: y }}>
     {damage}
@@ -121,21 +114,20 @@ function EatsApp({ setIsTabOpen }) {
     };
   }, [energy, energyMax, regenRate]);
 
-  // Добавляем эффект для автосохранения
+  // Новый useEffect для регулярного обновления данных
   useEffect(() => {
-    const saveInterval = setInterval(() => {
+    const updateInterval = setInterval(() => {
       UniverseData.saveToServer();
-    }, 30000); // Сохранение каждые 30 секунд
+    }, 10000); // Обновление каждые 10 секунд
 
-    // Добавляем обработчик для сохранения при закрытии или обновлении страницы
+    // Обработка прерывания сессии
     const handleBeforeUnload = () => {
       UniverseData.saveToServer();
     };
-
     window.addEventListener('beforeunload', handleBeforeUnload);
 
     return () => {
-      clearInterval(saveInterval);
+      clearInterval(updateInterval);
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
