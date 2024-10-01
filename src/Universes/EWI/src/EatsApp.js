c15c48cee5ae04644dd899bb0b3bc762725ac2e3
ValerySM <valerysokm@gmail.com>
2024-10-01 18:57:00 +0300

    Update EatsApp.js

--- src/Universes/EWI/src/EatsApp.js
+++ src/Universes/EWI/src/EatsApp.js
@@ -6,8 +6,8 @@ import UpgradeTab from './components/UpgradeTab';
 import BoostTab from './components/BoostTab';
 import TasksTab from './components/TasksTab';
 import SettingsButton from './components/SettingsButton';
-import clickerImage from '../public/clicker-image.png';
-import SoonTab from './components/SoonTab';
+import clickerImage from '../public/clicker-image.png'
+import SoonTab from './components/SoonTab'
 
 import {
   handleClick,
@@ -46,7 +46,6 @@ function EatsApp({ setIsTabOpen }) {
   const activityTimeoutRef = useRef(null);
   const clickerRef = useRef(null);
 
-  // Получение user_id из URL
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const userIdFromUrl = urlParams.get('user_id');
@@ -55,7 +54,6 @@ function EatsApp({ setIsTabOpen }) {
     }
   }, []);
 
-  // Функция для получения данных пользователя с бэкэнда
   const fetchUserData = async () => {
     if (!userId) return;
 
@@ -65,7 +63,6 @@ function EatsApp({ setIsTabOpen }) {
       const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user/${userId}`);
       if (response.ok) {
         const userData = await response.json();
-        // Обновление состояний данными с бэкэнда
         setTotalClicks(userData.totalClicks || 0);
         setEnergy(userData.energy || 1000);
         setEnergyMax(userData.energyMax || 1000);
@@ -80,17 +77,13 @@ function EatsApp({ setIsTabOpen }) {
       console.error('Error fetching user data:', error);
     }
 
-    setIsLoading(false); // Данные загружены
+    setIsLoading(false);
   };
 
-  // Выполнение запроса на получение данных пользователя после получения userId
   useEffect(() => {
-    if (userId) {
-      fetchUserData();
-    }
+    fetchUserData();
   }, [userId]);
 
-  // Функция для обновления данных пользователя
   const updateUserData = async (updates) => {
     if (!userId) return;
 
@@ -111,14 +104,26 @@ function EatsApp({ setIsTabOpen }) {
     }
   };
 
-  // Обновление количества кликов
   const updateTotalClicks = (additionalClicks) => {
     const newTotal = totalClicks + additionalClicks;
     setTotalClicks(newTotal);
     updateUserData({ totalClicks: newTotal });
   };
 
-  // Логика обработки кликов и отображения прогресса
+  const handleTabOpen = (tab) => {
+    setActiveTab(tab);
+    setIsTabOpenState(true);
+    setIsTabOpen(true);
+    setShowButtons(false);
+  };
+
+  const handleBackButtonClick = () => {
+    setActiveTab(null);
+    setIsTabOpenState(false);
+    setIsTabOpen(false);
+    setShowButtons(true);
+  };
+
   const handleInteraction = useCallback((e) => {
     e.preventDefault();
     setIsImageDistorted(true);
@@ -145,18 +150,18 @@ function EatsApp({ setIsTabOpen }) {
     if (activityTimeoutRef.current) {
       clearTimeout(activityTimeoutRef.current);
     }
-
+    
     activityTimeoutRef.current = setTimeout(() => {
       setIsImageDistorted(false);
     }, 200);
   }, [damageLevel, energy, count, totalClicks]);
 
-  // Добавляем обработчики событий на кликер
   useEffect(() => {
     const clicker = clickerRef.current;
     if (clicker) {
       clicker.addEventListener('click', handleInteraction);
       clicker.addEventListener('touchstart', handleInteraction, { passive: false });
+      
       return () => {
         clicker.removeEventListener('click', handleInteraction);
         clicker.removeEventListener('touchstart', handleInteraction);
@@ -164,7 +169,6 @@ function EatsApp({ setIsTabOpen }) {
     }
   }, [handleInteraction]);
 
-  // Логика автоматического восстановления энергии
   useEffect(() => {
     const interval = setInterval(() => {
       setEnergy(prevEnergy => {
@@ -183,7 +187,6 @@ function EatsApp({ setIsTabOpen }) {
     };
   }, [energy, energyMax, regenRate]);
 
-  // Контент для разных вкладок
   const tabContent = (() => {
     switch (activeTab) {
       case 'UPGRADE':
@@ -237,26 +240,25 @@ function EatsApp({ setIsTabOpen }) {
     }
   })();
 
-  // Процент оставшейся энергии
   const remainingEnergyPercentage = ((energyMax - energy) / energyMax) * 100;
 
-  // Если данные еще загружаются, отображаем сообщение о загрузке
   if (isLoading) {
-    return <div>Загрузка...</div>;
+    return <div>Loading...</div>;
   }
 
   return (
     <div className={`App`}>
       <header className="App-header">
-        <SettingsButton isActive={activeTab !== null} />
+        <SettingsButton isActive={activeTab !== null} /> 
         <div className="balance-container">
           <img src={clickerImage} alt="Balance Icon" className="balance-icon" />
           <p>{totalClicks}</p>
         </div>
         <div className="energy-container">
-          <p>Энергия: {Math.floor(energy)}/{energyMax}</p>
+          <p>Energy: {Math.floor(energy)}/{energyMax}</p>
         </div>
-        <div className="clicker-container" ref={clickerRef}>
+        <div className="clicker-container"
+             ref={clickerRef}>
           <img src={clickerImage} alt="Clicker" className={`clicker-image ${isImageDistorted ? 'distorted' : ''}`} />
           <div className="progress-circle" style={{ boxShadow: '0px 0px 10px 5px gray' }}>
             <CircularProgressbar
@@ -287,18 +289,18 @@ function EatsApp({ setIsTabOpen }) {
             </button>
             <button className={activeTab === 'SOON' ? 'active' : ''} onClick={() => handleTabOpen('SOON')}>
               REF
-            </button>
-          </div>
-        )}
-        {isTabOpenState && (
-          <div className={`tab-content ${isTabOpenState ? 'open' : ''}`}>
-            <button className="back-button" onClick={handleBackButtonClick}>Назад</button>
-            {tabContent}
-          </div>
-        )}
-      </header>
-    </div>
-  );
+			</button>
+         </div>
+       )}
+       {isTabOpenState && (
+         <div className={`tab-content ${isTabOpenState ? 'open' : ''}`}>
+           <button className="back-button" onClick={handleBackButtonClick}>Back</button>
+           {tabContent}
+         </div>
+       )}
+     </header>
+   </div>
+ );
 }
 
 export default EatsApp;
